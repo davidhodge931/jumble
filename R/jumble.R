2f593df7b4865be8f4aab8664ef04a2139334881
davidhodge931 <davidhodge931@gmail.com>
2026-02-25 08:55:19 +1300

    Update

--- R/jumble.R
+++ R/jumble.R
@@ -18,82 +18,62 @@ jumble <- c(
   "#CDC5BFFF"
 )
 
-#' Teal color
+#' Teal colour
 #' @export
 #' @examples
 #' scales::show_col(teal)
 teal <- "#0095A8FF"
 
-#' Orange color
+#' Orange colour
 #' @export
 #' @examples
 #' scales::show_col(orange)
 orange <- "#FFA600FF"
 
-#' Navy color
+#' Navy colour
 #' @export
 #' @examples
 #' scales::show_col(navy)
 navy <- "#003F5CFF"
 
-#' Red color
+#' Red colour
 #' @export
 #' @examples
 #' scales::show_col(red)
 red <- "#DA3C39FF"
 
-#' Pink color
+#' Pink colour
 #' @export
 #' @examples
 #' scales::show_col(pink)
 pink <- "#EC9ECBFF"
 
-#' Purple color
+#' grey colour
+#' @export
+#' @examples
+#' scales::show_col(grey)
+grey <- "#CDC5BFFF"
+
+#' Purple colour
 #' @export
 #' @examples
 #' scales::show_col(purple)
 purple <- "#67609CFF"
 
-#' Oat color
-#' @export
-#' @examples
-#' scales::show_col(oat)
-oat <- "#CDC5BFFF"
-
-#' Black color (from Flexoki base palette)
-#' @export
-#' @examples
-#' scales::show_col(black)
-black <- "#1C1B1Aff"
-
-#' Grey color
-#' @export
-#' @examples
-#' scales::show_col(grey)
-grey <- "#f2f2f2ff"
-
-#' Beige color (from Flexoki base palette)
-#' @export
-#' @examples
-#' scales::show_col(beige)
-beige <- "#F2F0E5ff"
-
-#' Slate color
+#' Slate colour
 #' @export
 #' @examples
 #' scales::show_col(slate)
 slate <- "#8991A1FF"
 
-#' Blue color
+#' Blue colour
 #' @export
 #' @examples
 #' scales::show_col(blue)
 blue <- "#4797C3FF"
 
-#' Ocean color
+#' Ocean colour
 #' @export
 #' @examples
 #' scales::show_col(ocean)
 ocean <- "#357BA2FF"
-
-
