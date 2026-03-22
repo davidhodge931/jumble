f2e4094ad15dbbdf0e99764763710bef1c6d17b9
davidhodge931 <davidhodge931@gmail.com>
2026-03-23 11:50:54 +1300

    Update

--- R/jumble.R
+++ R/jumble.R
@@ -1,11 +1,10 @@
 #' A discrete colour palette
 #'
 #' @description
-#' A pretty discrete colour palette that is also relatively colourblind and contrast safe on a light background.
+#' A pretty discrete colour palette that is also relatively colourblind and contrast safe.
 #'
 #' Note:
 #'
-#' * The palette is designed for a light background
 #' * The first 4 colours are colour-blind safe for deutanomaly, protanomaly and tritanomaly
 #' * The first 5 colours are colour-blind safe for deutanomaly
 #' * The first 3 colours are greyscale safe.
