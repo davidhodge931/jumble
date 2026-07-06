a4ff1c896885369d9c25fd6672f5aa747911475d
davidhodge931 <davidhodge931@gmail.com>
2026-07-07 09:45:27 +1200

    Update

--- tests/testthat/test-jumble.R
+++ tests/testthat/test-jumble.R
@@ -5,10 +5,18 @@ test_that("jumble is a character vector of 7 hex colours", {
 })
 
 test_that("jumble contains expected colours in order", {
-  expect_equal(jumble, c(
-    "#0095A8FF", "#FFA600FF", "#19526CFF", "#DA3C39FF",
-    "#EC9ECBFF", "#8991A1FF", "#CDC5BFFF"
-  ))
+  expect_equal(
+    jumble,
+    c(
+      "#0095A8FF",
+      "#FFA600FF",
+      "#19526CFF",
+      "#DA3C39FF",
+      "#EC9ECBFF",
+      "#8991A1FF",
+      "#CDC5BFFF"
+    )
+  )
 })
 
 test_that("individual colour objects are single hex strings", {
@@ -21,13 +29,13 @@ test_that("individual colour objects are single hex strings", {
 })
 
 test_that("individual colours match expected hex values", {
-  expect_equal(teal,   "#0095A8FF")
+  expect_equal(teal, "#0095A8FF")
   expect_equal(orange, "#FFA600FF")
-  expect_equal(navy,   "#19526CFF")
-  expect_equal(red,    "#DA3C39FF")
-  expect_equal(pink,   "#EC9ECBFF")
-  expect_equal(slate,  "#8991A1FF")
-  expect_equal(grey,   "#CDC5BFFF")
+  expect_equal(navy, "#19526CFF")
+  expect_equal(red, "#DA3C39FF")
+  expect_equal(pink, "#EC9ECBFF")
+  expect_equal(slate, "#8991A1FF")
+  expect_equal(grey, "#CDC5BFFF")
 })
 
 test_that("jumble is composed of the 7 individual colour objects", {
@@ -35,9 +43,9 @@ test_that("jumble is composed of the 7 individual colour objects", {
 })
 
 test_that("documented colourblind safe subsets are correct", {
-  expect_equal(jumble[1:4], c(teal, orange, navy, red))  # deutanomaly, protanomaly, tritanomaly safe
-  expect_equal(jumble[1:5], c(teal, orange, navy, red, pink))  # deutanomaly safe
-  expect_equal(jumble[c(1:4, 7)], c(teal, orange, navy, red, grey))  # 5-colour alternative
+  expect_equal(jumble[1:4], c(teal, orange, navy, red)) # deutanomaly, protanomaly, tritanomaly safe
+  expect_equal(jumble[1:5], c(teal, orange, navy, red, pink)) # deutanomaly safe
+  expect_equal(jumble[c(1:4, 7)], c(teal, orange, navy, red, grey)) # 5-colour alternative
 })
 
 test_that("all jumble colours are unique", {
